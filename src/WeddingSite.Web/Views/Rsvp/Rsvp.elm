58f72f96735debdbd7550a6c73ca456adac45f6f
gatkin <gregscottatkin@gmail.com>
2019-06-01 20:14:51 -0500

    Allow filtering by name

--- src/WeddingSite.Web/Views/Rsvp/Rsvp.elm
+++ src/WeddingSite.Web/Views/Rsvp/Rsvp.elm
@@ -1,7 +1,7 @@
 import Browser
 import Html exposing (Html, button, div, h1, input, text)
 import Html.Attributes exposing (class, id, placeholder, type_)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 
 
 main =
@@ -22,45 +22,65 @@ type alias Guest =
   , name: String
   , status: GuestStatus
   , isSelected: Bool
+  , isVisible: Bool
   }
+
+type alias PlusOnePair = (String, String)
+
+initialPlusOnes : List PlusOnePair
+initialPlusOnes =
+  [ ("John Doe", "Jane Doe")
+  , ("Philip J Fry", "Turanga Leela")
+  , ("Amy Wang", "Kif Kroker")
+  ]
 
 createGuest : String -> Int -> Guest
 createGuest name guestId =
-  { id = guestId, name = name, status = Unregistered, isSelected = False }
+  { id = guestId, name = name, status = Unregistered, isSelected = False, isVisible = True }
 
 initialGuests : List Guest
 initialGuests =
   [ createGuest "John Doe" 1
   , createGuest "Jane Doe" 2
   , createGuest "Philip J Fry" 3
-  , createGuest "Leela" 4
-  , createGuest "Zap" 5
-  , createGuest "Kip" 6
-  , createGuest "Hermes" 7
-  , createGuest "Bender" 8
-  , createGuest "Amy" 9
+  , createGuest "Turanga Leela" 4
+  , createGuest "Zapp Brannigan" 5
+  , createGuest "Kif Kroker" 6
+  , createGuest "Hermes Conrad" 7
+  , createGuest "Bender Bending Rodrigez" 8
+  , createGuest "Amy Wang" 9
   , createGuest "Zoidberg" 10
   ]
 
+
 type alias Model =
   { guests: List Guest
+  , plusOnes: List PlusOnePair
+  , searchString: String
   }
+
 
 init : Model
 init =
   { guests = initialGuests
+  , plusOnes = initialPlusOnes
+  , searchString = ""
   }
 
 
 -- UPDATE
 
 type Msg = SelectGuest GuestId
+  | NewSearch String
+
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     SelectGuest guestId ->
       onGuestSelected guestId model
+    NewSearch searchString ->
+      onNewSearch searchString model
 
 
 onGuestSelected : GuestId -> Model -> Model
@@ -79,13 +99,41 @@ selectGuestIfIdMatches guestId guest =
     guest
 
 
+onNewSearch : String -> Model -> Model
+onNewSearch searchString model =
+  let
+      cleanSearchString = searchString |> String.trim |> String.toLower 
+      newGuestList = model.guests |> filterGuestsOnSearchString cleanSearchString model.plusOnes
+  in
+    { model | searchString = cleanSearchString, guests = newGuestList }
+
+
+filterGuestsOnSearchString : String -> List PlusOnePair -> List Guest -> List Guest
+filterGuestsOnSearchString searchString plusOnes guestList =
+    guestList
+      |> List.map (setVisibleIfMatchesSearch searchString plusOnes)
+
+
+setVisibleIfMatchesSearch : String -> List PlusOnePair -> Guest -> Guest
+setVisibleIfMatchesSearch searchString plusOnes guest =
+  { guest | isVisible = doesGuestMatchSearchString searchString plusOnes guest }
+
+
+doesGuestMatchSearchString : String -> List PlusOnePair -> Guest -> Bool
+doesGuestMatchSearchString searchString plusOnes guest =
+  if String.isEmpty searchString || String.contains searchString (String.toLower guest.name) then
+    True
+  else
+    False
+
+
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div [ class "container-fluid" ]
   [ titleView
-  , searchBarView
+  , searchBarView model.searchString
   , guestListView model.guests
   ]
 
@@ -105,7 +153,9 @@ guestView guest =
 guestListView : List Guest -> Html Msg
 guestListView guests =
   let
-    guestViews = List.map guestView guests
+    guestViews = guests
+      |> List.filter (\guest -> guest.isVisible)
+      |> List.map guestView
   in
     div [ class "row" ]
     [ div [ class "col-2" ] []
@@ -115,14 +165,14 @@ guestListView guests =
   
 
 
-searchBarView : Html Msg
-searchBarView =
+searchBarView : String -> Html Msg
+searchBarView searchText =
   div [ class "row", id "search-bar-row" ]
   [ div [ class "col-3" ] []
   , div [ class "col-6" ]
-    [ input [ class "form-control", placeholder "Search" ] []
+    [ input [ class "form-control", placeholder "Search", onInput NewSearch ] []
     ]
-  , div [ class "col-3" ] []
+  , div [ class "col-3" ] [ text ("'" ++ searchText ++ "'") ]
   ]
 
 
