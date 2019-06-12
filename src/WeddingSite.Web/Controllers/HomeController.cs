345bdb52be315460b24ae48a1b23d87b31138aad
gatkin <gregscottatkin@gmail.com>
2019-06-12 18:17:33 -0500

    Remove Photos route

--- src/WeddingSite.Web/Controllers/HomeController.cs
+++ src/WeddingSite.Web/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace WeddingSite.Controllers
             return View();
         }
 
-        public IActionResult Photos()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
