ecde66596a4c7ff61c23f59dc9d901357ee70271
gatkin <gregscottatkin@gmail.com>
2019-06-11 22:32:34 -0500

    Fix push Heroku script

--- src/BuildScripts/push-heroku.sh
+++ src/BuildScripts/push-heroku.sh
@@ -1,6 +1,6 @@
 # Pushes a new image to the Heroku registry.
 HEROKU_REGISTRY=registry.heroku.com/mckayandgreg/web
 
-docker login -u _ -p $(HEROKU_API_KEY) $(HEROKU_REGISTRY) && \
-docker tag $(TAG):latest $(HEROKU_REGISTRY) && \
-docker push $(HEROKU_REGISTRY)
+docker login -u _ -p $HEROKU_API_KEY $HEROKU_REGISTRY && \
+docker tag wedding-site:latest $HEROKU_REGISTRY && \
+docker push $HEROKU_REGISTRY
